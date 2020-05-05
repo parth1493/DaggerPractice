df9796eadc050ccc6fff7f63c8279d323b7c25ea
Parth <parth1493@gmail.com>
2020-05-04 22:10:11 -0400

    Initial commit

---
+++ app/src/main/java/com/example/daggerpractice/di/AppComponent.java
@@ -0,0 +1,27 @@
+package com.example.daggerpractice.di;
+
+import android.app.Application;
+
+import com.example.daggerpractice.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}
