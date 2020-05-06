030a3627cda1188500c07a513f38fb6365e141cd
Parth Desai <parth1493@gmail.com>
2020-05-05 23:02:28 -0400

    added singleton

--- app/src/main/java/com/example/daggerpractice/di/AppComponent.java
+++ app/src/main/java/com/example/daggerpractice/di/AppComponent.java
@@ -4,15 +4,19 @@ import android.app.Application;
 
 import com.example.daggerpractice.BaseApplication;
 
+import javax.inject.Singleton;
+
 import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
+@Singleton
 @Component(
         modules = {
                 AndroidSupportInjectionModule.class,
-                ActivityBuilderModule.class
+                ActivityBuilderModule.class,
+                AppModule.class
         }
 )
 public interface AppComponent extends AndroidInjector<BaseApplication> {
