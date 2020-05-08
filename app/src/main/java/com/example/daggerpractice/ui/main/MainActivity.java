e925465afd0de63474d847ca365b1f099c562aa8
Parth Desai <parth1493@gmail.com>
2020-05-08 07:36:25 -0400

    added logout logic

--- app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.daggerpractice.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.daggerpractice.BaseActivity;
@@ -13,5 +16,24 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"Hello Sir I am in",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout:
+                sessionManager.logOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
